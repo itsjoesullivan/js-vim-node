caad008a789dfae906f75d6c15534a88f2d37d26
itsjoesullivan <itsjoesullivan@gmail.com>
2013-06-22 15:48:37 -0400

    mauve provides the colors

--- index.js
+++ index.js
@@ -10,24 +10,71 @@ program
 
 var Vim = require('../js-vim');
 
+var tui = require('../terminal-ui');
+
+var Rainbow = require('../rainbow/js/rainbow.js');
+require('../rainbow/js/language/javascript.js')(Rainbow);
+var mauve = require('mauve');
+
+mauve.set({
+
+	//generic
+	'keyword.operator': 'white',
+	'string': 'white',
+	'constant.character.escape': 'white',
+	'comment': 'white',
+	'constant.numeric': 'white',
+	'keyword': 'white',
+	'constant.language': 'white',
+	'keyword.operator': 'white',
+	'function.call': '#d7af5f',
+	'storage.function': '#d7af5f',
+	'entity.name.function': '#d7af5f',
+	
+
+
+	//js specific
+	'selector': 'white',
+	'support': 'white',
+	'support.property': 'white',
+	'support.function': 'white',
+	'support.method': 'white',
+	'support.tag.script': 'white',
+	'string': 'white',
+	'entity.tag.script': 'white',
+	'string.regexp': 'white',
+	'string.regexp.open': 'white',
+	'contstant.regexp.escape': 'white',
+	'string.regexp.close': 'white',
+	'string.regexp.modifier': 'white',
+	'storage': 'red',
+	'entity.function': 'white',
+	'keyword': 'white',
+	'function.anonymous': '#d7af5f',
+	'param': "#6B8E23"
+});
+
+
+
+
 	//Instance
 	var vim = new Vim();
 
-	//Create view
-	var View = require('./lib/view');
-	var view = new View({
-		vim: vim,
-		columns: program.columns,
-		lines: program.lines
-	});
 
 	//Handle keystrokes
 	Keys = require('../keys');
 	var keys = new Keys();
 
 	//Listen for changes. Delta acts as a hint (small change, big change, line change, etc.)
-	vim.on('change', function(delta) {
-		view.render(delta);
+
+	var _text = '';
+	tui.clear();
+	vim.view.on('change', function() {
+		var text = vim.text();
+		Rainbow.highlightBlockForLanguage(text, 'javascript', function(highlightedText) {
+			vim.view.text = highlightedText;
+			tui.write(vim.view.getText() );
+		});
 	});
 
 	//Connect keys to vim instance
