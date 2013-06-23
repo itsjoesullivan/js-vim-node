a87c3640223574db46384b3732ba92774af96092
itsjoesullivan <itsjoesullivan@gmail.com>
2013-06-23 18:14:04 -0400

    Refactor.

--- index.js
+++ index.js
@@ -6,94 +6,44 @@ program
 	.option('--lines <num>', 'Lines in display', parseInt)
 	.parse(process.argv);
 
+//Main app
+var Vim = require('../js-vim'),
+	//Renderer
+	tui = require('../terminal-ui');
 
-
-var Vim = require('../js-vim');
-
-
-var tui = require('../terminal-ui');
-
+//Syntax highlighter
 var Rainbow = require('../rainbow/js/rainbow.js');
+//Language
 require('../rainbow/js/language/javascript.js')(Rainbow);
+
+//Color scheme
 var mauve = require('mauve');
+var scheme = require('./lib/scheme');
+mauve.set(scheme);
 
-mauve.set({
+//Instance
+var vim = new Vim();
+//TODO let this be in the constructor
+vim.view.cols = program.columns;
+vim.view.lines = program.lines;
 
-	//generic
-	'keyword.operator': '#d7af5f',
-	'string': '#d7af5f',
-	'constant.character.escape': '#d7af5f',
-	'comment': '#d7af5f',
-	'constant.numeric': '#d7af5f',
-	'keyword': '#d7af5f',
-	'constant.language': '#d7af5f',
-	'keyword.operator': '#d7af5f',
-	'function.call': '#d7af5f',
-	'storage.function': '#d7af5f',
-	'entity.name.function': '#d7af5f',
-	
+//Apply node commands (file write, etc.)
+require('./lib/commands')(vim);
 
 
-	//js specific
-	'selector': '#d7af5f',
-	'support': '#d7af5f',
-	'support.property': '#d7af5f',
-	'support.function': '#d7af5f',
-	'support.method': '#d7af5f',
-	'support.tag.script': '#d7af5f',
-	'string': '#d7af5f',
-	'entity.tag.script': '#d7af5f',
-	'string.regexp': '#d7af5f',
-	'string.regexp.open': '#d7af5f',
-	'contstant.regexp.escape': '#d7af5f',
-	'string.regexp.close': '#d7af5f',
-	'string.regexp.modifier': '#d7af5f',
-	'storage': 'red',
-	'entity.function': '#d7af5f',
-	'keyword': '#d7af5f',
-	'function.anonymous': '#d7af5f',
-	'param': "#6B8E23"
+//Keystrokes
+Keys = require('../keys');
+var keys = new Keys();
+
+//Connect keys to vim instance
+keys.fn = function(key) {
+	vim.exec(key);
+};
+
+//Clear the terminal screen
+tui.clear();
+
+//Tie tui to vim.view
+vim.view.on('change', function() {
+	tui.write(vim.view.getText());
 });
-
-
-
-
-	//Instance
-	var vim = new Vim();
-
-	require('./lib/commands')(vim);
-
-
-	//Handle keystrokes
-	Keys = require('../keys');
-	var keys = new Keys();
-
-	//Listen for changes. Delta acts as a hint (small change, big change, line change, etc.)
-
-	var _text = '';
-	tui.clear();
-
-	//Let vim know how big screen is.
-	vim.view.cols = program.columns;
-	vim.view.lines = program.lines;
-	var tmpText = '';
-
-	vim.view.on('change', function() {
-		//Grab the text
-		var text = vim.text();
-		//Do syntax on it.
-		Rainbow.highlightBlockForLanguage(text, 'javascript', function(highlightedText) {
-			//Set as view
-			vim.view.text = highlightedText;
-			var viewText = vim.view.getText();
-//			console.log('are they the same?',tmpText === viewText);
-			tmpText = viewText;
-			//Render the text into a view and write it to screen
-			tui.write( viewText );
-		});
-	});
-
-	//Connect keys to vim instance
-	keys.fn = function(key) {
-		vim.exec(key);
-	};
